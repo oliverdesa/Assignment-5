b815b9895cc522619b4d413d115630dbd20b73b9
Oliver De Sa <oliver.desa@mail.utoronto.ca>
2022-10-27 14:15:31 -0400

    Added defensive programming IF statement, ensuring only one numeric value is entered

--- test.bird.data.R
+++ test.bird.data.R
@@ -8,3 +8,15 @@ source('C:/Users/Oliver/MSC1090/assignment5/Circle.Utilities.R')
 library(EnvStats)
 
 args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args != 1)){
+  cat('Please input only 1 argument')
+  stop()
+} else if (is.na(as.numeric(args))){
+  cat('Please input a numeric argument')
+  stop()
+} else {
+  
+}
+
+
